1fe3995fcbca133b3bef37d716be0b13a27f383a
fadhilmuniff <fadil123wew@gmail.com>
2020-06-30 09:47:55 +0700

    menabhakn list :

--- index.php
+++ index.php
@@ -8,5 +8,9 @@
 <body>
     <h1>php nih</h1>
     <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Neque sequi iure quam eius nisi sint officiis fuga inventore? Accusamus necessitatibus, accusantium adipisci consectetur exercitationem iste perspiciatis rerum qui magnam nobis.</p>
+    <ul>
+    <li>satu
+    </li>
+    </ul>
 </body>
 </html>
